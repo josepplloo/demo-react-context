4e3f9ab8f2403cdf18680fae12e219ba97f0aa4c
Jose Garcia <Jose.Garcia@teaminternational.com>
2021-02-11 16:15:04 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Header, { LABEL } from './components/Header';
 import { getStories } from './services/storiesService';
 import { useLazyRequest } from './hooks/useRequest.tsx';
 import { useEffect } from  'react';
 import { ThemeContext, themes } from './contexts/ThemeContext';
+import Routes from './components/Routes';
 
 function App() {
   
@@ -22,20 +21,7 @@ function App() {
       <div className="App">
         <Header />
         {LABEL}
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </div>
+        <Routes />
       </div>
     </ThemeContext.Provider>  
   );
---
+++ src/components/Routes/index.js
@@ -0,0 +1,63 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from '../../screens/Home';
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function Routes() {
+  return (
+<Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="*">
+            <p>404</p>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+
+}
+
+export default Routes;
